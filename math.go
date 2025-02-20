ddcde142e0b164b44696a8308270a809fcf0bdbc
Leandro Santos <ldss.leandro0212@gmail.com>
2025-02-20 17:16:29 -0300

    feat: :sparkles: add sum function

---
+++ math.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(soma(112, 10))
+}
+
+func soma(a int, b int) int {
+	return a + b
+}
+
+func sub(a int, b int) int {
+	return a - b
+}
+
+func mult(a int, b int) int {
+	return a * b
+}
+
+func div(a int, b int) int {
+	return a / b
+}
