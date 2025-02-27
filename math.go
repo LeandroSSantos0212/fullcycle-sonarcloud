5e353ae23eacd929905b5b268d2c1f07b2a7151c
Leandro Santos <ldss.leandro0212@gmail.com>
2025-02-27 11:27:27 -0300

    ci: Ajust in file math.go

--- math.go
+++ math.go
@@ -9,15 +9,3 @@ func main() {
 func soma(a int, b int) int {
 	return a + b
 }
-
-func sub(a int, b int) int {
-	return a - b
-}
-
-func mult(a int, b int) int {
-	return a * b
-}
-
-func div(a int, b int) int {
-	return a / b
-}
